17d4fceaf9b5fa28311b7799449cfd5a1a3dfb8b
Nate Poths <pothsn@gmail.com>
2019-08-03 12:14:29 -0500

    Made a spelling correction in the display name for a property on the UserTaste model and removed all periods.

--- BeerMatchBoxService/Models/UserTaste.cs
+++ BeerMatchBoxService/Models/UserTaste.cs
@@ -18,46 +18,46 @@ namespace BeerMatchBoxService.Models
 
         //QUIZ BELOW! These will be: Strongly disagree (1) / disagree (2) / Indifferent or I don't know (3) / agree (4) / strongly agree (5)
         //Flavor specific
-        [Display(Name ="I do not consider myself to be overly sensitive to bitterness.")]
+        [Display(Name ="I do not consider myself to be overly sensitive to bitterness")]
         [Range(typeof(int), "0", "10")]
         public int LikesBitter { get; set; }
 
-        [Display(Name = "I like like things with a fruity taste.")]
+        [Display(Name = "I like like things with a fruity taste")]
         [Range(typeof(int), "0", "10")]
         public int LikesFruity { get; set; }
 
-        [Display(Name = "I like like things with a sour taste.")]
+        [Display(Name = "I like like things with a sour taste")]
         [Range(typeof(int), "0", "10")]
         public int LikesSour { get; set; }
 
-        [Display(Name = "I like the taste and aroma of hops.")]
+        [Display(Name = "I like the taste and aroma of hops")]
         [Range(typeof(int), "0", "10")]
         public int LikesHoppy { get; set; }
 
         //New
-        [Display(Name = "I like maltiness.")]
+        [Display(Name = "I like maltiness")]
         [Range(typeof(int), "0", "10")]
         public int LikesMalty { get; set; }
 
         //Changed/New
-        [Display(Name = "I like chocolate!")]
+        [Display(Name = "I like chocolate")]
         [Range(typeof(int), "0", "10")]
         public int LikesChocolate { get; set; }
 
         //New
-        [Display(Name = "I like Coffee!")]
+        [Display(Name = "I like Coffee")]
         [Range(typeof(int), "0", "10")]
         public int LikesCoffee { get; set; }
 
-        [Display(Name = "I gravitate towards sweet things (not only consindering beer).")]
+        [Display(Name = "I gravitate towards sweet things (not only considering beer)")]
         [Range(typeof(int), "0", "10")]
         public int LikesSweet { get; set; }
 
-        [Display(Name = "I like special beers that often come with a higher ABV.")]
+        [Display(Name = "I like special beers that often come with a higher ABV")]
         [Range(typeof(int), "0", "10")]
         public int LikesStrong { get; set; }
 
-        [Display(Name = "I mind the ABV of beer and prefer something more sessionable.")]
+        [Display(Name = "I mind the ABV of beer and prefer something more sessionable")]
         [Range(typeof(int), "0", "10")]
         public int LikesSession { get; set; }
 
@@ -67,22 +67,22 @@ namespace BeerMatchBoxService.Models
 
         //Coloration specific
         //New
-        [Display(Name = "I enjoy pale beers (of straw or golden color).")]
+        [Display(Name = "I enjoy pale beers (of straw or golden color)")]
         [Range(typeof(int), "0", "10")]
         public int LikesPale { get; set; }
 
         //New
-        [Display(Name = "I enjoy beers of a middling color (of amber, brown or red).")]
+        [Display(Name = "I enjoy beers of a middling color (of amber, brown or red)")]
         [Range(typeof(int), "0", "10")]
         public int LikesMiddling { get; set; }
 
         //New
-        [Display(Name = "I enjoy dark beers (of dark brown, dark red, black or any other deep shade).")]
+        [Display(Name = "I enjoy dark beers (of dark brown, dark red, black or any other deep shade)")]
         [Range(typeof(int), "0", "10")]
         public int LikesDark { get; set; }
 
         //New
-        [Display(Name = "I enjoy barrel-aged beers.")]
+        [Display(Name = "I enjoy barrel-aged beers")]
         [Range(typeof(int), "0", "10")]
         public int LikesBarrelAged { get; set; }
 
@@ -90,99 +90,95 @@ namespace BeerMatchBoxService.Models
 
         //Style specific
         //New
-        [Display(Name = "I enjoy Lagers.")]
+        [Display(Name = "I enjoy Lagers")]
         [Range(typeof(int), "0", "10")]
         public int LikesLager { get; set; }
 
         //New
-        [Display(Name = "I enjoy Ales.")]
+        [Display(Name = "I enjoy Ales")]
         [Range(typeof(int), "0", "10")]
         public int LikesAle { get; set; }
 
-        [Display(Name = "I enjoy Pale Ales.")]
+        [Display(Name = "I enjoy Pale Ales")]
         [Range(typeof(int), "0", "10")]
         public int LikesPaleAle { get; set; }
 
-        [Display(Name = "I enjoy India  Pale Ales.")]
+        [Display(Name = "I enjoy India  Pale Ales")]
         [Range(typeof(int), "0", "10")]
         public int LikesIPA { get; set; }
 
         //New
-        [Display(Name = "I enjoy Extra Special Bitters.")]
+        [Display(Name = "I enjoy Extra Special Bitters")]
         [Range(typeof(int), "0", "10")]
         public int LikesESB { get; set; }
 
-        //[Display(Name = "I enjoy West Coast IPAs.")]
+        //[Display(Name = "I enjoy West Coast IPAs")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesWestCoastIPA { get; set; }
 
-        //[Display(Name = "I enjoy New Englad IPAs.")]
+        //[Display(Name = "I enjoy New Englad IPAs")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesNewEnglandIPA { get; set; }
 
-        [Display(Name = "I enjoy Stouts.")]
+        [Display(Name = "I enjoy Stouts")]
         [Range(typeof(int), "0", "10")]
         public int LikesStout { get; set; }
 
-        [Display(Name = "I enjoy Porters.")]
+        [Display(Name = "I enjoy Porters")]
         [Range(typeof(int), "0", "10")]
         public int LikesPorter { get; set; }
 
-        [Display(Name = "I enjoy Brown Ales.")]
+        [Display(Name = "I enjoy Brown Ales")]
         [Range(typeof(int), "0", "10")]
         public int LikesBrownAle { get; set; }
 
-        //[Display(Name = "I enjoy Ambers.")]
+        //[Display(Name = "I enjoy Ambers")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesAmber { get; set; }
 
-        [Display(Name = "I enjoy Red Ales.")]
+        [Display(Name = "I enjoy Red Ales")]
         [Range(typeof(int), "0", "10")]
         public int LikesRedAle { get; set; }
 
-        [Display(Name = "I enjoy Wheat Beers.")]
+        [Display(Name = "I enjoy Wheat Beers")]
         [Range(typeof(int), "0", "10")]
         public int LikesWheat { get; set; }
 
-        //[Display(Name = "I enjoy Hefeweizens.")]
+        //[Display(Name = "I enjoy Hefeweizens")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesHefeweizen { get; set; }
 
-        //[Display(Name = "I enjoy wheat Belgian Witbiers.")]
+        //[Display(Name = "I enjoy wheat Belgian Witbiers")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesBelgianWit { get; set; }
 
-        [Display(Name = "I enjoy Sours.")]
+        [Display(Name = "I enjoy Sours")]
         [Range(typeof(int), "0", "10")]
         public int LikesSourBeer { get; set; }
 
-        //[Display(Name = "I enjoy Wild Ales.")]
+        //[Display(Name = "I enjoy Wild Ales")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesWildSour { get; set; }
 
-        //[Display(Name = "I enjoy Berliner Weisses.")]
+        //[Display(Name = "I enjoy Berliner Weisses")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesBerlinerWeisseSour { get; set; }
 
-        //[Display(Name = "I enjoy Goses.")]
+        //[Display(Name = "I enjoy Goses")]
         //[Range(typeof(int), "0", "10")]
         //public int LikesGoseSour { get; set; }
 
-        [Display(Name = "I enjoy Saisons.")]
+        [Display(Name = "I enjoy Saisons")]
         [Range(typeof(int), "0", "10")]
         public int LikesSaison { get; set; }
 
-        [Display(Name = "I enjoy Belgian beer styles.")]
+        [Display(Name = "I enjoy Belgian beer styles")]
         [Range(typeof(int), "0", "10")]
         public int LikesBelgian { get; set; }
 
         //New
-        [Display(Name = "I enjoy German beer styles.")]
+        [Display(Name = "I enjoy German beer styles")]
         [Range(typeof(int), "0", "10")]
         public int LikesGerman { get; set; }
-
-        
-
-
     }
 }
